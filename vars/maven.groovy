20bfd6a301370b78c18703dba5d407b8bc9bfa71
preethi k <kpreethiyadav579@gmail.com>
2022-09-06 22:03:56 +0530

    adding

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -12,8 +12,12 @@ def lintcheck()
 def sonarcheck()
 { 
     sh '''
-    echo "hello"
-     '''
+    mvn --version
+    mvn clean compile
+    pwd && ls -lrt
+    sonar-scanner -Dsonar.host.url=http://172.31.5.148:9000 -Dsonar.login=admin -Dsonar.password=DevOps321 -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    #sonar-scanner -Dsonar.host.url=http://172.31.5.148:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    '''
 }
 //calling the function
 
